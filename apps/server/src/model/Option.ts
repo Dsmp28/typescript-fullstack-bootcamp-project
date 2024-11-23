4c6d34ffdb4468e214c21b293e3a85c82c61d9fd
DavidM <dsmonje@correo.url.edu.gt>
2024-11-23 11:15:23 -0600

    feat(product): crear modelo, rutas, servicio y controlador para Product
    Se implementaron los siguientes cambios:
    
    - Se creó el modelo Product con Prisma y sus relaciones.
    
    - Se desarrollaron las rutas para manejar el CRUD de Product.
    
    - Se implementó el servicio ProductService para la lógica de negocio.
    
    - Se creó el controlador ProductController para manejar las solicitudes HTTP.

---
+++ apps/server/src/model/Option.ts
@@ -0,0 +1,7 @@
+export type Option = {
+  id: number;
+  name: string;
+  productId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
---
+++ apps/server/src/model/Variant.ts
@@ -0,0 +1,10 @@
+export type Variant = {
+  id: number;
+  name: string;
+  sku: string;
+  stock: number;
+  price: number;
+  productId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
